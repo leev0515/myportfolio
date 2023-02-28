376d02ddd95d1377d4519c6f0ebe4ebdc58dc088
leev0515 <106923528+leev0515@users.noreply.github.com>
2023-02-28 16:38:05 -0500

    Add files via upload

---
+++ src/Aboutme2.js
@@ -0,0 +1,14 @@
+
+
+
+function Abouutme2 () {
+    return (
+        <nav className="aboutme2">
+            <p>Before beginning my new career path with technology, I graduated with a Bachelor of Science in Elementary Eduaction in 2018.</p>
+            <p>When it was time to shift focus in my career, I thought  it would be amazing to combine my knowledge and passion for coding into a profession.</p>
+            <p>They say you never work a day in your life when you love what you do!</p>
+        </nav>
+    );
+}
+
+export default Abouutme2
