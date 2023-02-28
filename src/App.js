5adc20e422a4c7ae4a237fe87acb9928d7eb7a34
leev0515 <106923528+leev0515@users.noreply.github.com>
2023-02-28 16:39:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import Home from './Home';
+import Nav from './Nav';
 import Aboutme from './Aboutme';
+import Aboutme2 from './Aboutme2';
 import Contacts from './Contacts';
 import './App.css';
+import Threecolumns from './Threecolumns';
+import Oregon from './img/Oregon.jpg';
+
 
 function App() {
   return (
     <>
-      <Home />
+    <div className='bg-image'
+      style={{ backgroundImage: `url(${Oregon})` }}
+    >
+      <Nav />
       <Aboutme />
+      <Threecolumns />
+      <Aboutme2 />
       <Contacts />
+    </div>
+
     </>
   );
 }
