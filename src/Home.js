5b714ee2d26e768784d433fb879f72754e2653ee
leev0515 <106923528+leev0515@users.noreply.github.com>
2023-02-28 16:40:17 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,14 @@ import Homeicon from './img/Homeicon.png';
 function Nav() {
     return (
        <nav>
-            <ul className='icon'>
-                <li className="icon"><Link to="./"><img src={Homeicon} alt="Home Page icon by icons8" /></Link></li>
-            </ul>
             <ul className="home">
+                <li><Link to="./"><img src={Homeicon} alt="Home Page icon by icons8" /></Link></li>
                 <li><Link to="./">Home</Link></li>
                 <li><Link to="./aboutme">About Me</Link></li>
                 <li><Link to="./projects">Projects</Link></li>
                 <li><Link to="./resume">Resume</Link></li>
                 <li><Link to="./contacts">Contacts</Link></li>
-            </ul>
+                </ul>
        </nav>
     );
 }
