88dd6683ef660c1b8041e8247dc29a0d975099d5
leev0515 <106923528+leev0515@users.noreply.github.com>
2023-02-28 16:41:09 -0500

    Add files via upload

---
+++ src/Threecolumns.js
@@ -0,0 +1,20 @@
+function Threecolumns () {
+    return (
+        <div className='row'>
+            <div className='column'>
+                <h2>Column 1</h2>
+                <p>Project/Skill 1</p>
+            </div>
+            <div className='column'>
+                <h2>Column 2</h2>
+                <p>Project/Skill 2</p>
+            </div>
+            <div className="column">
+                <h2>Column 3</h2>
+                <p>Project/Skill 3</p>
+            </div>
+        </div>
+    );
+}
+
+export default Threecolumns;
